805d5d0c3f404fa090d6b8786d58be0aba4cf0ba
Richard <rsasubs@gmail.com>
2024-02-20 16:57:26 +1000

    Updated App.jsx to import Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Register from './Register.jsx'
+import Login from './Login.jsx'
 
 function App() {
   return (
     <section className="w-full flex flex-col h-screen items-center content-center justify-center bg-slate-50">
-      <Register />
+      <Login />
     </section>
   )
 }
