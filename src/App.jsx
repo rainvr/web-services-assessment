a168d4d74829b2c7898146f1e2cc2d5bc96b6a97
Richard <rsasubs@gmail.com>
2024-02-20 10:37:03 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return <>
+    <div className="navbar bg-slate-400">
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+    </div>
+    <input className=""></input>
+  </>
+}
+
+export default App
