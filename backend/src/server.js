774999320011f2d0c74d3a513ce104fc4ad6d447
Richard <rsasubs@gmail.com>
2024-03-01 14:35:01 +1000

    Updated server.js with cors, json parsing & the user controller route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,25 @@
 import express from "express"
+import cors from "cors"
+import userController from "./controllers/users.js"
 
 const app = express()
 const port = 8080
 
-app.use("/",(req,res)=>{
-    res.send("hello world")
-})
+// Enable Cross Origin Resource Sharing
+app.use(cors({
+    // Allow all origins
+    origin: true,
+}))
+
+// Enable JSON request parsing middleware
+app.use(express.json())
+
+// Import and use routes defined by the controllers
+app.use("/users", userController)
+
+// TODO: Catch errors raised by endpoints
+
+// Start listening for API requests
 app.listen(port, ()=>{
     console.log("server is listening on port http://localhost:"+ port)
 })
