fd40f371dd67c73144f9200dc8969ec21ff28857
Richard <rsasubs@gmail.com>
2024-03-01 10:02:27 +1000

    Updated model user.js with a few get models (different async structures) that were tested and work

--- backend/src/models/users.js
+++ backend/src/models/users.js
@@ -33,6 +33,64 @@ export function getAllUsers() {
         }))
 }
 
-getAllUsers().then(result => {
-    return console.log(result)
-})
+// Get a user by their ID
+export function getUserById(userId) {
+    return db.query("SELECT * FROM users WHERE user_id = ?", userId)
+        .then((([queryResult]) => {
+            if (queryResult.length > 0) {
+                return queryResult.map(
+                    result => newUser(
+                        result.user_id,
+                        result.user_email,
+                        result.user_password,
+                        result.user_role,    
+                        result.user_phone,
+                        result.user_firstname,
+                        result.user_lastname,
+                        result.user_address
+                    )
+                )
+            } else {
+                console.log(`Error - No users with user_id: ${userId}`)
+                // return
+            }
+        }))
+        .catch(error => {
+            console.log(`Error getting the user: ${error}`)
+        })
+}
+
+// getUserById(2).then(result => {
+//     return console.log(result)
+// })
+
+// Get a user by their email
+export async function getUserByEmail(userEmail) {
+    try {
+        const [queryResult] = await db.query("SELECT * FROM users WHERE user_email = ?", userEmail)
+        if (queryResult.length > 0) {
+            return queryResult.map(
+                result => newUser(
+                    result.user_id,
+                    result.user_email,
+                    result.user_password,
+                    result.user_role,    
+                    result.user_phone,
+                    result.user_firstname,
+                    result.user_lastname,
+                    result.user_address
+                )
+            )
+        } else {
+            return console.log(`Error - No users with user_email: ${userEmail}`)
+        }
+    } catch {
+        (error) => {
+            console.log(`Error getting the user: ${error}`)
+        }
+    }
+}
+
+// getUserByEmail("manager@email.com").then(result => {
+//     return console.log(result)
+// })
