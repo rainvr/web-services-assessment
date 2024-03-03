84f863eb0ae0c11569d665da2b726f88264862f3
Richard <rsasubs@gmail.com>
2024-03-03 11:55:10 +1000

    Added working getLogin function to users.js controller

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -5,15 +5,33 @@ import * as Users from "../models/users.js"
 
 const userController = Router()
 
-userController.post("/login", (req, res) => {
+// userController.post("/login", (req, res) => {  // TODO: reinstate this over next line when frontend is ready for login
+userController.post("/login", getLogin)
+
+export function getLogin(req, res) {    
     let loginData = req.body
 
     // TODO: need input validation
 
-    Users.getUserByEmail(loginData.email) 
-        .then(user => { console.log(user) })
-})
+    Users.getUserByEmail(loginData.email).then(user => {
+        // TODO: need bcrypt compare.Sync (in if then structure)
+        res.status(200).json({
+            status: 200,
+            message: "The user information has been returned!",
+            user
+        })
+    }).catch(error => {
+        res.status(400).json({
+            status: 400,
+            message: "login failed"
+        })
+    })
 
+}
+// )
+
+
+// Get all users
 userController.get("/", async (req, res) => {
     const users = await Users.getAllUsers()
 
@@ -21,6 +39,18 @@ userController.get("/", async (req, res) => {
         status: 200,
         message: "All Users List",
         users
+    })
+})
+
+// Get the user by their email
+userController.get("/profile/:email", async (req, res) => {
+    const userEmail = req.params.email
+    const user = await Users.getUserByEmail(userEmail)
+
+    res.status(200).json({
+        status: 200,
+        message: "The user has been returned!",
+        user
     })
 })
 
