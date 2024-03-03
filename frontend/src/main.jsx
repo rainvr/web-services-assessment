5c929e0e96d812bfd1fc7cea43b07dc7553325a1
Richard <rsasubs@gmail.com>
2024-03-03 11:53:50 +1000

    Put in some TODOs for user Authentication in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Router from "./router.jsx"
+// import { AuthenticationProvider } from './features/authentication.jsx'
 
+// TODO: enable the AuthenticationProvider below
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    {/* <AuthenticationProvider router={router}> */}
+      <Router />
+    {/* </AuthenticationProvider> */}
   </React.StrictMode>,
 )
