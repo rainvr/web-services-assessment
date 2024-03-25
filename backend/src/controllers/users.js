80e18ba145494bf6e19f599ef827c105da8f958d
Richard <rsasubs@gmail.com>
2024-03-25 15:26:05 +1000

    Login controller.  Tested ok.

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -2,6 +2,7 @@ import { Router } from "express"
 import * as Users from "../models/users.js"
 import pkg from 'lodash'
 const { isEmpty } = pkg
+import { v4 as uuid4 } from "uuid"
 
 const userController = Router()
 
@@ -20,27 +21,37 @@ export function getLogin(req, res) {
         })
     } else {  // If there is a request body
 
+        // TODO: Validate request body
+
         Users.getByEmail(loginData.email).then(user => {
 
-            // TODO: need bcrypt compare.Sync (in if then structure)
-            // TODO: compare passwords
-            
-            if (user) {  // If a matching user object is found
-            res.status(200).json({
-                status: 200,
-                message: "The user information has been returned",
-                user
-                })
-            } else {  // If no matching user object is found
-                res.status(404).json({
+            // If no matching user object is found
+            if (!user) {
+                return res.status(404).json({
                     status: 404,
                     message: "The user was not found"
+                })
+            }
+
+            // If a matching user object is found check the passwords match
+            if (loginData.email == user.email) {  // TODO: bcrypt hashsync this
+                user.authKey = uuid4().toString()  // create a unique auth key
+
+                // console.log("user" + JSON.stringify(user, null, 2)) // TODO: remove this test
+
+                Users.updateById(user).then(result => {
+                    res.status(200).json({
+                        status: 200,
+                        message: "The user is logged in",
+                        authenticationKey: user.authKey,
+                    })
                 })
             }
         }).catch(error => {
             res.status(400).json({
                 status: 400,
-                message: "login failed"
+                message: "login failed",
+                error
             })
         })
     } 
@@ -83,6 +94,8 @@ userController.post("/register", (req, res) => {
             message: "Missing request body"
         })
     } else {  // If there is a request body
+
+        // TODO: Validate request body
 
         // Convert the user data into an User model object
         const userObject = Users.newUser(
