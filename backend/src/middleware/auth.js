47b9a3d3ad4b16be1e4198dcd774fd46eaab97b0
Richard <rsasubs@gmail.com>
2024-04-01 16:33:11 +1000

    Added auth.js and RestrictedRoute.jsx

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,36 @@
+
+import * as user from "../models/users.js"
+
+export default function auth(allowed_roles) {
+    return function (req, res, next) {
+
+        const authenticationKey = req.get("X-AUTH-KEY")
+
+        if (authenticationKey) {
+
+            user.getByAuthKey(authenticationKey)
+                .then(user => {
+                    if (allowed_roles.includes(user.role)) {
+                        next()
+                    } else {
+                        res.status(403).json({
+                            status: 403,
+                            message:
+                                "Access forbidden",
+                        });
+                    }
+                })
+                .catch(error => {
+                    res.status(401).json({
+                        status: 401,
+                        message: "Authentication key invalid",
+                    });
+                })
+        } else {
+            res.status(401).json({
+                status: 401,
+                message: "Authentication key missing",
+            });
+        }
+    };
+}
---
+++ frontend/src/features/RestrictedRoute.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom"
+import { useAuthentication } from "../features/authentication"
+// import { Header } from "./components/Header.jsx"  // TODO: reinstate this, and below <Header /> ?
+
+export function RestrictedRoute({ allowedRoles = [], children }) {
+    const [user, login, logout] = useAuthentication()
+    const navigate = useNavigate()
+
+    const userIsAuthorised = user && allowedRoles.includes(user.role)
+
+    return userIsAuthorised
+        ? children
+        : <>
+            {/* <Header /> */}
+            <div className="flex flex-col justify-center items-center gap-4">
+                <h2 className="text-4xl">Not Authorised</h2>
+                <span className="text-xl">Access role is not permitted to view this page.</span>
+                <button className="btn btn-lg" onClick={() => navigate(-1)}>Back</button>
+            </div>
+        </>
+}
