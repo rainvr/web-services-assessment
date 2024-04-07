b5d611dff1dafd5b8ff4e9b3a2e3472db83601d3
Richard <rsasubs@gmail.com>
2024-04-07 20:35:40 +1000

    Added api/blogs.js update and delete. Not tested from frontend.

--- frontend/src/api/blogs.js
+++ frontend/src/api/blogs.js
@@ -45,3 +45,53 @@ export async function create(blog, authenticationKey) {
 
     return APIResponseObject
 }
+
+
+// ---------- UPDATE ---------- //
+
+/**
+ * PATCH /blogs
+ * Updates a blog post by its id in the database
+ * @param {Object} blog
+ * @param {String} authenticationKey
+ * @returns {Promise<Array<Object>>}
+ */
+export async function update(blog, authenticationKey) {
+    const apiResponse = await fetch(API_URL + "/blogs", {
+        method: "PATCH",
+        headers: {
+            'Content-Type': "application/json",
+            'X-AUTH-KEY': authenticationKey
+        },
+        body: JSON.stringify(blog)
+    })
+
+    const APIResponseObject = await apiResponse.json()
+
+    return APIResponseObject
+}
+
+// ---------- DELETE ---------- //
+
+/**
+ * DELETE /blogs/:id
+ * @param { String } blogId 
+ * @returns {Promise<Object>}
+ */
+export async function deleteById(blogId, authenticationKey) {
+    console.log(blogId)
+    console.log(authenticationKey)
+
+    const response = await fetch(API_URL + "/blogs/" + blogId, 
+        {
+            method: "DELETE",
+            headers: {
+                'Content-Type': "application/json",
+                'X-AUTH-KEY': authenticationKey
+            }
+        })
+
+    const APIResponseObject = await response.json()
+
+    return APIResponseObject
+}
