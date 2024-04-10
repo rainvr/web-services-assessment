6e94201562bf1e233c2b34e8550a2e2b5c0951ee
Richard <rsasubs@gmail.com>
2024-04-10 12:55:23 +1000

    Added getClass() controller and model. tested ok.

--- backend/src/controllers/classes.js
+++ backend/src/controllers/classes.js
@@ -7,7 +7,7 @@ import validator from "validator"
 
 const classController = Router()
 
-// Get all blogs
+// Get all classes
 classController.get("/", async (req, res) => {
     try {
         const classes = await Classes.getAll()
@@ -28,6 +28,33 @@ classController.get("/", async (req, res) => {
         return res.status(500).json({
             status: 500,
             message: "Error getting the classes",
+            error
+        })
+    }
+})
+
+// Get the classes for the next week
+classController.get("/weekly/:weekStartDate", async (req, res) => {
+    try {
+        const weekStartDate = req.params.weekStartDate
+        const classes = await Classes.getWeek(weekStartDate)
+
+        if (isEmpty(classes)) {
+            return res.status(404).json({
+                status: 404,
+                message: "There were no classes found"
+            })
+        }
+        
+        res.status(200).json({
+            status: 200,
+            message: "The classes for the week are listed",
+            classes
+        })
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            message: "Error getting the weekly classes",
             error
         })
     }
--- backend/src/models/classes.js
+++ backend/src/models/classes.js
@@ -1,13 +1,17 @@
 import { db } from "../database.js";
+import { addHours } from "date-fns"
 
 // --------- CONSTRUCTOR ---------- //
 
 // Construct a new class object
-export function newClass(id, datetime, weekday, locationId, locationName, activityId, activityName, activityDescription, activityDuration, trainerId, trainerName) {
+export function newClass(id, datetime, date, weekday, week, time, locationId, locationName, activityId, activityName, activityDescription, activityDuration, trainerId, trainerName) {
     return {
         id,
-        datetime,
+        datetime: addHours(new Date(datetime), 10),
+        date,
         weekday,
+        week,
+        time,
         locationId,
         locationName,
         activityId,
@@ -23,7 +27,7 @@ export function newClass(id, datetime, weekday, locationId, locationName, activi
 
 // Get all the classes from the database
 export function getAll() {
-    return db.query(`SELECT class_id, class_datetime, WEEKDAY(class_datetime) AS class_weekday, location_id, location_name, activity_id, activity_name, activity_description, activity_duration, user_id AS trainer_id, CONCAT(user_firstname, " ", user_lastname) AS trainer_name 
+    return db.query(`SELECT class_id, class_datetime, DATE_FORMAT(class_datetime, "%D %M") AS class_date, WEEKDAY(class_datetime) AS class_weekday, WEEK(class_datetime) AS class_week, class_time, location_id, location_name, activity_id, activity_name, activity_description, activity_duration, user_id AS trainer_id, CONCAT(user_firstname, " ", user_lastname) AS trainer_name 
     FROM classes, users, location, activities
     WHERE classes.class_trainer_user_id = users.user_id AND classes.class_location_id = location.location_id AND classes.class_activity_id = activities.activity_id`)
         .then((([queryResult]) => {
@@ -31,7 +35,40 @@ export function getAll() {
                 result => newClass(
                     result.class_id,
                     result.class_datetime,
+                    result.class_date,
                     result.class_weekday,
+                    result.class_week,
+                    result.class_time,
+                    result.location_id,
+                    result.location_name,
+                    result.activity_id,    
+                    result.activity_name,
+                    result.activity_description,
+                    result.activity_duration,
+                    result.trainer_id,
+                    result.trainer_name
+                )
+            )
+        }))
+        .catch(error => {
+            console.log(`Error getting all the classes: ` + error)
+        })
+}
+
+// Get the classes for the next week from the database
+export function getWeek(weekStartDate) {
+    return db.query(`SELECT class_id, class_datetime, DATE_FORMAT(class_datetime, "%D %M") AS class_date, WEEKDAY(class_date) AS class_weekday, WEEK(class_date) AS class_week, class_time, location_id, location_name, activity_id, activity_name, activity_description, activity_duration, user_id AS trainer_id, CONCAT(user_firstname, " ", user_lastname) AS trainer_name 
+    FROM classes, users, location, activities
+    WHERE classes.class_trainer_user_id = users.user_id AND classes.class_location_id = location.location_id AND classes.class_activity_id = activities.activity_id AND classes.class_datetime >= ? AND classes.class_datetime < ?`, [weekStartDate, addHours(weekStartDate,168)])
+        .then((([queryResult]) => {
+            return queryResult.map(
+                result => newClass(
+                    result.class_id,
+                    result.class_datetime,
+                    result.class_date,
+                    result.class_weekday,
+                    result.class_week,
+                    result.class_time,
                     result.location_id,
                     result.location_name,
                     result.activity_id,    
