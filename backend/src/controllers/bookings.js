0e78bd5318c47396f351a13c79a09ea4ce53cdfd
Richard <rsasubs@gmail.com>
2024-04-12 14:42:12 +1000

    Bookings delete and getAll models & controllers. Tested OK.

--- backend/src/controllers/bookings.js
+++ backend/src/controllers/bookings.js
@@ -8,6 +8,35 @@ const { isEmpty } = pkg
 const bookingController = Router()
 
 
+// GET /:userId
+// Get the bookings by the userId
+bookingController.get("/:userId", auth(["member"]), async (req, res) => {
+    try {
+        const userId = req.params.userId
+        const bookings = await Bookings.getAll(userId)
+
+        if (isEmpty(bookings)) {
+            return res.status(404).json({
+                status: 404,
+                message: "There were no bookings found"
+            })
+        }
+        
+        res.status(200).json({
+            status: 200,
+            message: "The bookings for the user are returned",
+            bookings
+        })
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            message: "Error getting the bookings for the user",
+            error
+        })
+    }
+})
+
+
 // POST /
 // Create the booking
 bookingController.post("/", auth(["member"]), async (req, res) => {
@@ -79,6 +108,48 @@ bookingController.post("/", auth(["member"]), async (req, res) => {
         res.status(500).json({
             status: 500,
             message: "Booking failed: " + error,
+            error
+        })
+    }
+})
+
+
+// DELETE /
+bookingController.delete("/:id", auth(["member"]), async (req, res) => {
+    try {
+        // Get the id
+        const bookingId = req.params.id
+
+        // If no param is found
+        if (isEmpty(bookingId)) {
+            return res.status(404).json({
+                status: 400,
+                message: "Missing ID in the request"
+            })
+        }
+
+        // Validate the bookingId
+        if (!/^(0|[1-9]\d*)$/.test(bookingId)) {
+            // Show error
+            return res.status(400).json({
+                status: 400,
+                message: "Invalid booking ID format"
+            })
+        }
+
+        // Delete the booking object in the db with matching ID
+        const response = await Bookings.deleteById(bookingId)
+        
+        .then(response => {
+                res.status(200).json({
+                    status: 200,
+                    message: "Booking Deleted"
+                })
+            })
+    } catch (error) {
+        res.status(500).json({
+            status: 500,
+            message: "Delete failed",
             error
         })
     }
--- backend/src/models/bookings.js
+++ backend/src/models/bookings.js
@@ -3,32 +3,63 @@ import { db } from "../database.js"
 // --------- CONSTRUCTOR ---------- //
 
 // Construct a new booking object
-export function newBooking(id, userId, classId, createDate) {
+export function newBooking(id, createDate, userId, trainerName, classId, classDatetime, activityName, activityDescription) {
     return {
         id,
+        createDate,
         userId,
         classId,
-        createDate
+        classDatetime,
+        activityName,
+        activityDescription,
+        trainerName,
     }
 }
 
 // --------- READ ---------- //
 
 // Get all the bookings from the database
-// export function getAll() {
-//     return db.query(`SELECT location_id, location_name FROM location`)
-//         .then((([queryResult]) => {
-//             return queryResult.map(
-//                 result => newLocation(
-//                     result.location_id,
-//                     result.location_name
-//                 )
-//             )
-//         }))
-//         .catch(error => {
-//             console.log(`Error getting the location: ` + error)
-//         })
-// }
+export function getAll(userId) {
+    // return db.query(`SELECT booking_id, booking_created_datetime, user_id, CONCAT(user_firstname, " ", user_lastname) AS trainer_name, class_id, class_datetime, activity_name, activity_description 
+    // FROM bookings, users, classes, activities
+    // WHERE bookings.booking_user_id = ? AND bookings.booking_class_id = classes.class_id AND classes.class_activity_id = activities.activity_id`, userId)
+    return db.query(`
+        SELECT 
+            bookings.booking_id, 
+            bookings.booking_created_datetime, 
+            users.user_id, 
+            classes.class_id, 
+            classes.class_datetime,
+            activities.activity_name,
+            activities.activity_description,
+            CONCAT(trainer.user_firstname, ' ', trainer.user_lastname) AS trainer_name
+        FROM 
+            bookings
+            INNER JOIN users ON bookings.booking_user_id = users.user_id
+            INNER JOIN classes ON bookings.booking_class_id = classes.class_id
+            INNER JOIN activities ON classes.class_activity_id = activities.activity_id
+            INNER JOIN users AS trainer ON classes.class_trainer_user_id = trainer.user_id
+        WHERE 
+            bookings.booking_user_id = ?
+        `, userId)    
+        .then((([queryResult]) => {
+            return queryResult.map(
+                result => newBooking(
+                    result.booking_id, 
+                    result.booking_created_datetime, 
+                    result.user_id, 
+                    result.class_id, 
+                    result.class_datetime, 
+                    result.activity_name, 
+                    result.activity_description,
+                    result.trainer_name
+                )
+            )
+        }))
+        .catch(error => {
+            console.log(`Error getting all the bookings: ` + error)
+        })
+}
 
 
 // --------- CREATE ---------- //
@@ -48,4 +79,11 @@ export async function create(booking) {
     .then(([result]) => {
         return { ...booking, id: result.insertId }  // return an object with the booking fields plus the inserted ID as the id
     })
+}
+
+
+// --------- DELETE ---------- //
+// Delete the user by ID
+export async function deleteById(bookingId) {
+    return db.query("DELETE FROM bookings WHERE booking_id = ?", bookingId)
 }
