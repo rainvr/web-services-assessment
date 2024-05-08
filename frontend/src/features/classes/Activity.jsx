9a0b6f2c437e21a9aacb4800e716d515e8e7e766
Richard <rsasubs@gmail.com>
2024-05-08 15:12:29 +1000

    Hid the book button for past dates

--- frontend/src/features/classes/Activity.jsx
+++ frontend/src/features/classes/Activity.jsx
@@ -1,7 +1,6 @@
-import { format } from "date-fns"
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useAuthentication } from "../authentication"
+import { isPast } from "date-fns"
 
 
 function Activity({clazz}) {
@@ -13,7 +12,7 @@ function Activity({clazz}) {
             <td></td>
             <td>{clazz.activityName}</td>
             <td>{clazz.activityDuration} minutes</td>
-            { user && user.role === "member" && <td><button type="submit" onClick={() => navigate(`/create-booking`, {state: {clazz}})} className="badge badge-outline font-semibold text-blue-600 hover:bg-blue-200 focus:bg-blue-200 active:bg-blue-200">Book</button></td> }
+            { user && user.role === "member" && !isPast(clazz.date) && <td><button type="submit" onClick={() => navigate(`/create-booking`, {state: {clazz}})} className="badge badge-outline font-semibold text-blue-600 hover:bg-blue-200 focus:bg-blue-200 active:bg-blue-200">Book</button></td> }
       </tr>
     )
 }
